327c74eab3d3916483555dbd3bfe9008f670c50f
KTraister <ktraiste@ford.com>
2018-10-16 13:01:28 -0400

    go fmt'd

--- src/server/file_server.go
+++ src/server/file_server.go
@@ -1,14 +1,16 @@
 package main
+
 import (
-  "net/http"
+	"net/http"
 )
+
 func ping(w http.ResponseWriter, r *http.Request) {
-  w.Write([]byte("pong"))
+	w.Write([]byte("pong"))
 }
 func main() {
-  http.Handle("/", http.FileServer(http.Dir("./files")))
-  http.HandleFunc("/ping", ping)
-  if err := http.ListenAndServe(":8080", nil); err != nil {
-    panic(err)
-  }
+	http.Handle("/", http.FileServer(http.Dir("./files")))
+	http.HandleFunc("/ping", ping)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
